5244679d01130db9746e30cc833ec54f0b9bccda
ossa03 <kfcxd953pelo@gmail.com>
2024-08-06 12:50:14 +0900

    LINE_TODO_APPでwebアプリケーションとして導入したしたURLを更新した。

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,42 @@
-import React, { useCallback, useState } from 'react'
-import Header from './Header'
-import Form from './Form'
-import LogBox from './LogBox'
+import React, { useCallback, useState } from "react"
+import Header from "./Header"
+import Form from "./Form"
+import LogBox from "./LogBox"
 
 export default function App() {
 	// state
-	const [log, setLog] = useState('')
+	const [log, setLog] = useState("")
 
 	const handleSubmit = useCallback(({ todo, date, time }) => {
 		// GAS APIへデータを送信する(LINE_TODO_APPでwebアプリケーションとして導入したしたURL)
-		const GAS_API_URL = `https://script.google.com/macros/s/AKfycbwqOLNkBPoszr-pcng9ThgT-1R8wjQi4cJU1CZamocY8JNYkm36/exec`
+		// const GAS_API_URL = `https://script.google.com/macros/s/AKfycbwqOLNkBPoszr-pcng9ThgT-1R8wjQi4cJU1CZamocY8JNYkm36/exec`
+		// URLは2024/08/06に更新した。
+		const GAS_API_URL = `https://script.google.com/macros/s/AKfycbx9HvlZetTwKklrZkC4ZilBfyrUs_ei0KogjnmcPJDfrWrNLozuqwVSTs5NSHcFBjp1/exec`
 
-		console.log('prePOST: \n\n\n ')
+		console.log("prePOST: \n\n\n ")
 
 		// Send a POST request
 		fetch(GAS_API_URL, {
-			method: 'POST',
-			mode: 'no-cors',
-			'Content-Type': 'application/x-www-form-urlencoded',
+			method: "POST",
+			mode: "no-cors",
+			"Content-Type": "application/x-www-form-urlencoded",
 			body: JSON.stringify({
 				todo,
 				date,
 				time,
 			}),
-			credentials: 'include',
+			credentials: "include",
 		})
 			.then((res) => {
 				console.log(res)
-				console.log('GASのLINE_TODO_APPのWeb_APIへ送信しました')
-				setLog('GASのLINE_TODO_APPのWeb_APIへ送信しました')
+				console.log("GASのLINE_TODO_APPのWeb_APIへ送信しました")
+				setLog("GASのLINE_TODO_APPのWeb_APIへ送信しました")
 			})
-			.catch((error) => console.log('Error:  ', error.message))
+			.catch((error) => console.log("Error:  ", error.message))
 	}, [])
 
 	return (
-		<div className='App'>
+		<div className="App">
 			<Header />
 			<Form handleSubmit={handleSubmit} />
 			<LogBox log={log} />
